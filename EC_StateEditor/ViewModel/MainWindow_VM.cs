5d6333da44aea705a1439b52b9958b5db30c38b8
SuxrobGM <suxrobGM@gmail.com>
2018-09-18 22:29:24 +0500

    added base files

---
+++ EC_StateEditor/ViewModel/MainWindow_VM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+using Prism.Commands;
+using EC_StateEditor.Model;
+
+namespace EC_StateEditor.ViewModel
+{
+    class MainWindow_VM
+    {
+
+    }
+}
